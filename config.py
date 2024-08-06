79a42aa908af1ca87c575c18ba9e9a699e368e9d
3x-dev <aryan.singhal2016@gmail.com>
2024-08-05 17:34:18 -0700

    db add

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL', 'postgres://default:YCtJ8bjhv7za@ep-calm-credit-a4qi4i6r-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
+    SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql+psycopg2://default:YCtJ8bjhv7za@ep-calm-credit-a4qi4i6r-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
